96c514d91454c61c45358369ff9a320b346f6ce1
illia kozlov <samsung.id.od@gmail.com>
2023-07-08 13:18:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//const one = 'one';
+//const two = 'two';
+//const three = 'three';
+//const result = `${one} ${two} ${three}`;
+//console.log (result)
+
+//{
+//    const str = String(12345);
+//    str.split(""); 
+//}
